02ae3a7e25134f45b2d30431b0d436cdfe62af01
Jose Palacios <jmpd1988@gmail.com>
2022-02-11 15:35:04 -0500

    adding clearer HTTP error messages

--- lib/client/build.ts
+++ lib/client/build.ts
@@ -21,7 +21,27 @@ export const buildClient = ({config}: Config) => {
   })
 
   conn.interceptors.response.use((response) => response, (error) => {
-    throw error
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      const data = error.response.data
+      const status = error.response.status
+      //const headers = error.response.headers
+      const errmsg = `${status} -> ${data.error}`
+      throw new Error(errmsg)
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      const request = error.request
+      throw new Error(`Error on request: ${JSON.stringify(request)}`)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      const message = error.message
+      throw new Error(`Error on setting request: ${JSON.stringify(message)}`)
+    }
+    //throw new Error(`Something happened: ${error.config}`)
+    //console.log(error.config);
   })
 
   return conn
